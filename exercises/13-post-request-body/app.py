f3801aa01414aac0a389305e081e801f52a52e28
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:34:36 +0100

    POST request body

--- exercises/13-post-request-body/app.py
+++ exercises/13-post-request-body/app.py
@@ -1,4 +1,12 @@
+# EJERCICIO 13 - POST request body
 import requests
-
-response = requests.post("https://assets.breatheco.de/apis/fake/sample/save-project-json.php")
+url = "https://assets.breatheco.de/apis/fake/sample/save-project-json.php"
+myobj = {
+    "id": 2323,
+    "title": "Very big project"
+}
+headers = {
+    "Content-Type": "application/json"
+}
+response = requests.post(url, json = myobj, headers = headers)
 print(response.text)
