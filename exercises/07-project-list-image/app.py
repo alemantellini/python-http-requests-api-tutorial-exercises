f4a4d9dd0ebdcb3b8effa4d65cb7ef18d8eeab4e
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:30:53 +0100

    Project List Image

--- exercises/07-project-list-image/app.py
+++ exercises/07-project-list-image/app.py
@@ -1,3 +1,9 @@
+# EJERCICIO 7 - Project List Image
 import requests
-
-# Your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/project_list.php")
+if response.status_code == 200:
+    body = response.json()
+    project_url = body[2]["images"][2]
+    print(project_url)
+else:
+    print("Something went wrong")
