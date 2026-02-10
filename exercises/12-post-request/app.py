fcc9747cd80cb14dc431d6aed30ef9fbb17499a2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:33:53 +0100

    POST request
    Added code to send a POST request and print the response.

--- exercises/12-post-request/app.py
+++ exercises/12-post-request/app.py
@@ -1,3 +1,6 @@
+# EJERCICIO 12 - POST request
 import requests
-
 # Your code here
+url = "https://assets.breatheco.de/apis/fake/sample/post.php"
+response = requests.post(url)
+print(response.text)
