866346969af171619f5dd5370973f27f8e8004a6
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:28:51 +0100

    JSON response

--- exercises/04-response-body-json/app.py
+++ exercises/04-response-body-json/app.py
@@ -1,4 +1,11 @@
+# EJERCICIO 4 - Response JSON
 import requests
-
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/time.php")
-print(response.text)
+if response.status_code == 200:
+    time = response.json()
+    hours = time["hours"]
+    minutes = time["minutes"]
+    seconds = time["seconds"]
+    print(f"Current time: {hours} hrs {minutes} min and {seconds} sec")
+else:
+    print("Something went wrong")
