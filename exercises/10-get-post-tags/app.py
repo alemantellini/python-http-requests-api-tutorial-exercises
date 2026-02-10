0c7dd37288fe101ae7add56f6225e67ee2906fcd
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:32:49 +0100

    Get post tags from API
    Implement function to fetch tags for a given post ID from an API.

--- exercises/10-get-post-tags/app.py
+++ exercises/10-get-post-tags/app.py
@@ -1,8 +1,13 @@
+# EJERCICIO 10 - Get post tags
 import requests
-
 def get_post_tags(post_id):
     # Your code here
-    return None
-
-
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    if response.status_code == 200:
+        body = response.json()
+        for post in body["posts"]:
+            if post["id"] == post_id:
+                return post["tags"]
+    else:
+        print("Something went wrong")
 print(get_post_tags(146))
