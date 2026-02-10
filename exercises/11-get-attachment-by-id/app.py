441cc0b985de3e1715370027bbe25dddf6d06444
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:33:16 +0100

    Implement get_attachment_by_id function
    Fetch attachment title by ID from external API.

--- exercises/11-get-attachment-by-id/app.py
+++ exercises/11-get-attachment-by-id/app.py
@@ -1,7 +1,15 @@
+# EJERCICIO 11 - Get attachment by id
 import requests
-
 def get_attachment_by_id(attachment_id):
     # Your code here
-    return None
-
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    if response.status_code == 200:
+        body = response.json()
+        for post in body["posts"]:
+            if "attachments" in post:
+                for attachment in post["attachments"]:
+                    if attachment["id"] == attachment_id:
+                        return attachment["title"]
+    else:
+        print("Something went wrong")
 print(get_attachment_by_id(137))
