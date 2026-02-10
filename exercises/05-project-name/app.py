6acf0cc08898bafbd7443d17dfa75a983882aece
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:29:28 +0100

    Fetch and print project name from API

--- exercises/05-project-name/app.py
+++ exercises/05-project-name/app.py
@@ -1,3 +1,9 @@
+# EJERCICIO 5 - Project name
 import requests
-
-# Your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/project1.php")
+if response.status_code == 200:
+    body = response.json()
+    project_name = body["name"]
+    print(project_name)
+else:
+    print("Something went wrong")
