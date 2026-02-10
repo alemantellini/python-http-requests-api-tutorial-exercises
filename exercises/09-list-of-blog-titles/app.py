0e2036875e2f9bc2b144ccd6f3f1ea15cb55fe18
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:31:56 +0100

    Implement get_titles function to retrieve blog titles
    Fetch blog titles from an API and return them as a list.

--- exercises/09-list-of-blog-titles/app.py
+++ exercises/09-list-of-blog-titles/app.py
@@ -1,8 +1,16 @@
+# EJERCICIO 9 - List of blog titles
 import requests
-
 def get_titles():
     # Your code here
-    return None
-
-
+    titles = []
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    if response.status_code == 200:
+        body = response.json()
+        for post in body["posts"]:
+            title = post["title"]
+            if title:
+                titles.append(title)
+    else:
+        print("Something went wrong")
+    return titles
 print(get_titles())
