3e5fe4eaa843d3b09732abefe06cd59476f30153
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 22:31:22 +0100

    Post blog author

--- exercises/08-blog-post-author/app.py
+++ exercises/08-blog-post-author/app.py
@@ -1,3 +1,9 @@
+# EJERCICIO 8 - Post blog author
 import requests
-
-# Your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+if response.status_code == 200:
+    body = response.json()
+    author = body["posts"][0]["author"]["name"]
+    print(author)
+else:
+    print("Something went wrong")
